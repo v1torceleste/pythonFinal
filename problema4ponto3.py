ecb8ad68039198211a511090fe80e0f49654ed4b
v1torceleste <vitorsouza0409@gmail.com>
2026-03-26 20:26:02 -0300

    adicionando tabuada e contagem

---
+++ problema4ponto3.py
@@ -0,0 +1,8 @@
+n = int(input("Até que número você deseja contar? "))
+
+num = 1
+while num <= n:
+    print(num)
+    num = num + 1
+
+print("Cabô os numero, agora vo voltar pro colo do meu namorado")
